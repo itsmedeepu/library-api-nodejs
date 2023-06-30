20cb4291bb257302021f0c47d4d404733ed44f95
deepak <deepak.github@outlook.com>
2023-06-30 09:34:21 +0530

    implemented all methods

--- controller/LibraryController.js
+++ controller/LibraryController.js
@@ -1,55 +1,125 @@
-const Book = require("../model/books")
-const {getResponseStructure}=require("../assets/responseStructure")
-const saveBook=async(req,res)=>{
-    const book=new Book(req.body);
-    try{
-       await book.save().then((ressult)=>{
-        const resp=getResponseStructure(req.body,200,"data inserted Successfully");
-        res.send(resp)
-       }).catch((err)=>{
-        console.log("values error"+err)
-       })
-    }catch(err){
+const Book = require("../model/books");
+const { getResponseStructure } = require("../assets/responseStructure");
+const saveBook = async (req, res) => {
+  const book = new Book(req.body);
+  try {
+    await book
+      .save()
+      .then((result) => {
+        const resp = getResponseStructure(
+          req.body,
+          200,
+          "data inserted Successfully"
+        );
+        res.send(resp);
+      })
+      .catch((err) => {
+        console.log("values error" + err);
+      });
+  } catch (err) {
+    const resp = getResponseStructure(
+      null,
+      500,
+      "something went bad at server"
+    );
+    res.send(resp);
+  }
+};
 
-        res.send("something went bad at server side")
-        console.log(err)
-    }
-}
+const updateBook = (req, res) => {
+  console.log(req, body);
+};
 
-const updateBook=(req,res)=>{
-    console.log(req,body)
-}
+const deleteBook = (req, res) => {
+  console.log(req.param.id);
+};
 
-const deleteBook=(req,res)=>{
-    console.log(req.param.id)
-}
+const getAllBooks = async (req, res) => {
+  try {
+    await Book.find({})
+      .then((data) => {
+        const resp = getResponseStructure(
+          data,
+          200,
+          "data fetched sucessfully"
+        );
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.send("some error");
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const getAllBooks=(req,res)=>{
+const getBooKById = async (req, res) => {
+  try {
+    await Book.findOne({ _id: req.params.id })
+      .then((data) => {
+        const resp = getResponseStructure(
+          data,
+          200,
+          "data fetched sucessfully"
+        );
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.send(getResponseStructure(null, 404, "no data found for this id"));
+      });
+  } catch (err) {
+    res.send("something went bad at server side bro");
+  }
+};
 
-}
+const getBooksByAuthor = async (req, res) => {
+  try {
+    await Book.find({ authorname: req.params.authname })
+      .then((data) => {
+        data.sort((a, b) => a.bookname.localeCompare(b.bookname));
+        const resp = getResponseStructure(
+          data,
+          200,
+          "data fetched sucessfully"
+        );
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.send(
+          getResponseStructure(null, 404, "no books for this authorname")
+        );
+      });
+  } catch (err) {
+    res.send("someting went bad at server");
+  }
+};
 
-const getBooKById=(req,res)=>{
+const getBookByName = async (req, res) => {
+  try {
+    await Book.find({ bookname: req.params.bookname })
+      .then((data) => {
+        data.sort((a, b) => a.bookname.localeCompare(b.bookname));
+        const resp = getResponseStructure(
+          data,
+          200,
+          "data fetched sucessfully"
+        );
+        res.send(resp);
+      })
+      .catch((err) => {
+        res.send(getResponseStructure(null, 404, "no books found by name"));
+      });
+  } catch (err) {
+    res.send("someting went bad at server");
+  }
+};
 
-
-}
- 
-
-const getBooksByAuthor=(req,res)=>{
-
-}
-
-const getBookByName=(req,res)=>{
-
-}
-
-
-
-module.exports={
-    saveBook,
-    updateBook,
-    deleteBook,
-    getAllBooks,
-    getBooKById,
-    getBooksByAuthor,
-    getBookByName
-}
+module.exports = {
+  saveBook,
+  updateBook,
+  deleteBook,
+  getAllBooks,
+  getBooKById,
+  getBooksByAuthor,
+  getBookByName,
+};
--- routes/library.js
+++ routes/library.js
@@ -1,37 +1,31 @@
-const express = require("express")
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
 
 //database connection
 
-const db = require("../conn/conn")
+const {
+  saveBook,
+  updateBook,
+  deleteBook,
+  getBooKById,
+  getAllBooks,
+  getBooksByAuthor,
+  getBookByName,
+} = require("../controller/LibraryController");
 
-const {saveBook,updateBook,deleteBook, getBooKById, getAllBooks, getBooksByAuthor, getBookByName}=require("../controller/LibraryController")
+router.get("/home", (req, res) => {
+  res.send("library home page ");
+});
 
-router.get("/home",(req,res)=>{
+router.post("/savebook", saveBook);
 
-    res.send("library home page ")
+router.put("/updateBook/:id");
 
-})
+router.delete("/delete/:id", deleteBook);
 
+router.get("/getBookById/:id", getBooKById);
+router.get("/getAllBooks", getAllBooks);
+router.get("/getBookByAuthor/:authname", getBooksByAuthor);
+router.get("/getBookByName/:bookname", getBookByName);
 
-router.post("/savebook",saveBook);
-
-router.put("/updateBook/:id")
-
-router.delete("/delete/:id",deleteBook)
-
-router.get("/getBookById/:id",getBooKById)
-router.get("/getAllBooks",getAllBooks)
-router.get("/getBookByAuthor",getBooksByAuthor)
-router.get("/getBookByName",getBookByName)
-
-
-
-
-
-
-
-
-
-
-module.exports=router
+module.exports = router;
